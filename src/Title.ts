b92231510f7c905fed13c28d987af32d60caaa52
Shridhar Sharma <shridharkaushik29@gmail.com>
2019-03-03 16:19:58 +0530

    v1.0.5

---
+++ src/Title.ts
@@ -0,0 +1,57 @@
+import * as React from "react";
+import * as _ from "lodash";
+import {UIRouterReact} from "@uirouter/react";
+
+export default class Title {
+
+    appName = "React App";
+    private _onChange: ((title: string) => void)[] = []
+    private _formatTitle: (title: string) => string
+
+    constructor({appName, onChange, formatter}: { appName?: string, onChange?: (title: string) => void, formatter?: (title: string) => string } = {}) {
+        this.setAppName(appName)
+        this.onChange(onChange);
+        this.setFormatter(formatter)
+    }
+
+    setAppName(name: string): Title {
+        this.appName = name;
+        return this;
+    }
+
+    setFormatter(formatter: (title: string) => string = title => `${title} | ${this.appName}`) {
+        this._formatTitle = formatter;
+        return this;
+    }
+
+    getAppName() {
+        return this.appName;
+    }
+
+    onChange(onChange: (title: string) => void = () => null): () => void {
+        this._onChange.push(onChange)
+        return () => {
+            _.pull(this._onChange, onChange);
+        }
+    }
+
+    set(title: string): this {
+        document.title = this._formatTitle(title);
+        this._onChange.forEach(callback => callback(title));
+        return this;
+    }
+
+    hookUiRouter(router: UIRouterReact): this {
+        router.transitionService.onSuccess({}, transition => {
+            const state = transition.to();
+            const title = _.get(state, "data.title", 'Untitled');
+            if (title !== false)
+                this.set(title || '')
+        })
+        return this;
+    }
+
+    get(): string {
+        return document.title;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,3 @@
-import {UIRouterReact} from "@uirouter/react";
-import * as _ from "lodash";
-
-export class Title {
-
-    appName = "React App";
-
-    formatTitle: (title: string) => string = title => `${title} | ${this.appName}`
-
-    constructor({appName}) {
-        this.setAppName(appName)
-    }
-
-    setAppName(name): Title {
-        this.appName = name;
-        return this;
-    }
-
-    set(title): Title {
-        document.title = this.formatTitle(title);
-        return this;
-    }
-
-    hookUiRouter(router: UIRouterReact) {
-        router.transitionService.onSuccess({}, transition => {
-            const state = transition.to();
-            const title = _.get(state, "data.title", 'Untitled');
-            this.set(title)
-        })
-    }
-
-    get(): string {
-        return document.title;
-    }
-}
+export import TitleProvider = require("./TitleProvider");
+export import TitleContext = require("./TitleContext");
+export import Title = require("./Title");
